ff35028d3e93fa699eaa10b07db1fe10dcb89806
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-16 13:49:14 +0700

    hello

---
+++ practice8/hello.php
@@ -0,0 +1,12 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- 21102126 - MM4 -->
+
+<?php
+
+$nama = "Muhammad Reyhan Asshidiq Djajasasmita";
+$kelas = "MM4";
+echo "Hello $nama <br>";
+print "Class $kelas";
+
+?>
+

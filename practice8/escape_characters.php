d85f025c46623d6fb871a7ba3957f7799beb590f
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-16 13:48:46 +0700

    escape-characters

---
+++ practice8/escape_characters.php
@@ -0,0 +1,28 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- 21102126-MM4 -->
+
+<?php
+
+//! Escape Character
+/* To insert characters that are illegal in a string, use an escape character.
+*/
+
+$x = "We are the so-called \"Vikings\" from the north.";
+
+echo $x;
+
+
+//* Other Escape Characters :
+/* 
+    \' : single quote
+    \" : double quote
+    \$ : PHP variables
+    \n : new line
+    \r : carriage return
+    \t : tab
+    \f : form feed
+    \ooo : octal value
+    \xhh : hex value
+*/
+
+?>

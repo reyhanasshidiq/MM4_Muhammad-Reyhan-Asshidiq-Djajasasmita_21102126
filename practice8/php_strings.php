884065f9c7ddd5e10fc055f6bf748affc0005ef6
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-16 13:50:10 +0700

    php-strings

---
+++ practice8/php_strings.php
@@ -0,0 +1,31 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- // 21102126 - MM4 -->
+
+<?php
+echo "Hello";
+echo "<br>";
+echo 'Hello';
+
+// Double Quote
+echo "<br>";
+$x = "John";
+echo "Hello $x";
+
+// Single Quote
+echo "<br>";
+$x = "John";
+echo 'Hello $x';
+
+// String Length
+echo "<br>";
+echo strlen("Hello world!");
+
+// Word Count
+echo "<br>";
+echo str_word_count("Hello world!");
+
+// Search For Text Within a String
+echo "<br>";
+echo strpos("Hello world!", "world");
+
+?>

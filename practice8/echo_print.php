d66cd41536acfb5caafd49a07a83018fb409bb9a
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-16 13:48:11 +0700

    echo-print

---
+++ practice8/echo_print.php
@@ -0,0 +1,24 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- 21102126 - MM4 -->
+
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello Reyhan !<br>";
+echo "Bismillah Trying to learn PHP !<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+echo  "<br>";
+
+echo  "<br>";
+echo  "<br>";
+
+$txt1 = "Learn PHP";
+$txt2 = "Web";
+$x = 5;
+$y = 4;
+
+print "<h2>" . $txt1 . "</h2>";
+print "Study PHP at " . $txt2 . "<br>";
+print "Start from ";
+print $x + $y;
+
+?>

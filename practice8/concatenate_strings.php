90043a2dc45ef9dcee8b990b911b8f88f0b8297d
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-16 13:47:09 +0700

    concat

---
+++ practice8/concatenate_strings.php
@@ -0,0 +1,27 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- // 21102126 - MM4 -->
+
+<?php
+
+// String Concatenation
+$x = "Hello";
+$y = "World";
+$z = $x . $y;
+echo $z;
+
+// Adding space
+echo "<br>";
+$x = "Hello";
+$y = "World";
+$z = $x . " " . $y;
+echo $z;
+
+// Easier way by using double quotes
+echo "<br>";
+$x = "Hello";
+$y = "World";
+$z = "$x $y";
+echo $z;
+
+
+?>

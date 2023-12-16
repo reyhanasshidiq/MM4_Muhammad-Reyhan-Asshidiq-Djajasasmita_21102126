7b63628c871559ec8dc0497a36d763ea8fc1561e
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-16 13:46:37 +0700

    comment

---
+++ practice8/comment.php
@@ -0,0 +1,18 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- 21102126 - MM4 -->
+
+
+<?php
+// Ini adalah Single-line comment
+
+# Ini juga adalah single-line comment
+
+/* Ini adalah
+
+Multi-line Comment*/ 
+
+$nama = "Reyhan Asshidiq"; #Ini Adalah Nama
+$kelas = "MM4";
+echo "Hello $nama <br>";    // Memanggil variabel nama
+print "Class $kelas";       // Memanggil variabel kelas
+?>

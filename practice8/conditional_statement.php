18876f908666cf48eacb179f770566fb91ac1335
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-17 09:52:49 +0700

    conditional-statement

---
+++ practice8/conditional_statement.php
@@ -0,0 +1,21 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- 21102126 / MM4 -->
+
+<?php
+
+//* Contoh 1
+if (5 > 3) {
+    echo "Have a Good Day";
+}
+
+//* Contoh 2
+echo ("<br>");
+$t = 14;
+
+if ($t < 20) {
+    echo "Have a good day!";
+}
+
+
+
+?>

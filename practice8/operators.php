329a0877c6c1f3d90ed0c86413b817bab5d5c3dc
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-17 09:55:55 +0700

    operators

---
+++ practice8/operators.php
@@ -0,0 +1,47 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- 21102126 / MM4 -->
+
+<?php
+//* Addition
+$a = 5;
+$b = 10;
+$c = $a + $b;
+echo($c);
+
+//* Substraction
+echo("<br>");
+$a = 5;
+$b = 10;
+$c = $a - $b;
+echo($c);
+
+//* Multiplication
+echo("<br>");
+$a = 5;
+$b = 10;
+$c = $a * $b;
+echo($c);
+
+//* Division
+echo("<br>");
+$a = 5;
+$b = 10;
+$c = $a / $b;
+echo($c);
+
+//* Modulus
+echo("<br>");
+$a = 5;
+$b = 10;
+$c = $a % $b;
+echo($c);
+
+//* Exponentiation
+echo("<br>");
+$a = 5;
+$b = 10;
+$c = $a ** $b;
+echo($c);
+
+
+?>

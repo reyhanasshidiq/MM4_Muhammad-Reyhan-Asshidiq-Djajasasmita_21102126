fdadb4d675f2b71bde3e467580b9792e8870744d
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-17 14:22:37 +0700

    array

---
+++ practice8/array.php
@@ -0,0 +1,11 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- 21102126 / MM4 -->
+
+<?php
+
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+echo("<br>");
+echo count($cars);
+
+?>

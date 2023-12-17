0f2c70f03e25aa8a9cc9f178f0be7d8290738615
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-17 14:23:13 +0700

    indexed-array

---
+++ practice8/indexed_array.php
@@ -0,0 +1,20 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- 21102126 / MM4 -->
+
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+echo ("<br>");
+
+
+//* Looping Array
+echo ("<br>");
+$cars = array("Volvo", "BMW", "Toyota");
+$arrlength = count($cars);
+
+for ($x = 0; $x < $arrlength; $x++) {
+    echo $cars[$x];
+    echo "<br>";
+}
+
+?>

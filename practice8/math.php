ba8a1a9872dd8a32771362994f835691dbb1ad10
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-17 09:54:55 +0700

    math

---
+++ practice8/math.php
@@ -0,0 +1,47 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- 21102126 / MM4 -->
+
+<?php
+echo(pi());
+
+?>
+
+<br>
+<?php
+//* min() and max() functions 
+echo(min(0, 150, 30, 20, -8, -200));
+echo("<br>");
+echo(max(0, 150, 30, 20, -8, -200));
+
+?>
+
+<br>
+<?php
+//* PHP abs() function
+echo(abs(-6.7));
+
+?>
+
+<br>
+<?php
+//* PHP sqrt() function
+echo(sqrt(64));
+
+?>
+
+<br>
+<?php
+//* PHP round() function
+echo(round(0.60));
+echo(round(0.49));
+
+?>
+
+<br>
+<?php
+//* PHP round() function
+echo(rand());
+echo("<br>");
+echo(rand(10, 100));
+
+?>

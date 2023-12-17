95887c9874a4e7796bc1212313eddbfb3d900e2f
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-17 14:24:03 +0700

    superglobal

---
+++ practice8/superglobal.php
@@ -0,0 +1,33 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- 21102126 / MM4 -->
+
+<?php
+
+//! PHP $GLOBALS
+$x = 75;
+$y = 25;
+
+function addition()
+{
+    $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+}
+
+addition();
+echo $z;
+
+
+//! PHP $SERVER
+echo ("<br>");
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+
+?>

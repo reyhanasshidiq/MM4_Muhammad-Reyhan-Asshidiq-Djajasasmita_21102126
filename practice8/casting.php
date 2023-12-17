5da89c799dd888495e0a3876f3f711bf160da273
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-17 09:52:17 +0700

    practice8

---
+++ practice8/casting.php
@@ -0,0 +1,201 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- 21102126 -->
+
+<!-- 
+(string) - Converts to data type String
+(int) - Converts to data type Integer
+(float) - Converts to data type Float
+(bool) - Converts to data type Boolean
+(array) - Converts to data type Array
+(object) - Converts to data type Object 
+-->
+
+<?php
+//* Cast to String
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (string) $a;
+$b = (string) $b;
+$c = (string) $c;
+$d = (string) $d;
+$e = (string) $e;
+
+//To verify the type of any object in PHP, use the var_dump() function:
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
+
+?>
+
+<br>
+<?php
+//* Cast to Integer
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "25 kilometers"; // String
+$d = "kilometers 25"; // String
+$e = "hello"; // String
+$f = true;    // Boolean
+$g = NULL;    // NULL
+
+$a = (int) $a;
+$b = (int) $b;
+$c = (int) $c;
+$d = (int) $d;
+$e = (int) $e;
+$f = (int) $f;
+$g = (int) $g;
+
+?>
+
+
+<br>
+<?php
+//* Cast to Float
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "25 kilometers"; // String
+$d = "kilometers 25"; // String
+$e = "hello"; // String
+$f = true;    // Boolean
+$g = NULL;    // NULL
+
+$a = (float) $a;
+$b = (float) $b;
+$c = (float) $c;
+$d = (float) $d;
+$e = (float) $e;
+$f = (float) $f;
+$g = (float) $g;
+
+?>
+
+<br>
+<?php
+//* Cast to Float
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = 0;       // Integer
+$d = -1;      // Integer
+$e = 0.1;     // Float
+$f = "hello"; // String
+$g = "";      // String
+$h = true;    // Boolean
+$i = NULL;    // NULL
+
+$a = (bool) $a;
+$b = (bool) $b;
+$c = (bool) $c;
+$d = (bool) $d;
+$e = (bool) $e;
+$f = (bool) $f;
+$g = (bool) $g;
+$h = (bool) $h;
+$i = (bool) $i;
+
+?>
+
+
+<br>
+<?php
+//* Cast to Array
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (array) $a;
+$b = (array) $b;
+$c = (array) $c;
+$d = (array) $d;
+$e = (array) $e;
+
+?>
+
+
+<br>
+<?php
+//* Converting Objects into Arrays
+
+class Car
+{
+    public $color;
+    public $model;
+    public function __construct($color, $model)
+    {
+        $this->color = $color;
+        $this->model = $model;
+    }
+    public function message()
+    {
+        return "My car is a " . $this->color . " " . $this->model . "!";
+    }
+}
+
+$myCar = new Car("red", "Volvo");
+
+$myCar = (array) $myCar;
+var_dump($myCar);
+
+?>
+
+
+<br>
+<?php
+//* Cast to Objects
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (object) $a;
+$b = (object) $b;
+$c = (object) $c;
+$d = (object) $d;
+$e = (object) $e;
+
+?>
+
+<br>
+<?php
+//* Converting Arrays into Objects :
+
+$a = array("Volvo", "BMW", "Toyota"); // indexed array
+$b = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43"); // associative array
+
+$a = (object) $a;
+$b = (object) $b;
+
+?>
+
+<br>
+<?php
+//* Casting to NULL
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (unset) $a;
+$b = (unset) $b;
+$c = (unset) $c;
+$d = (unset) $d;
+$e = (unset) $e;
+
+?>
---
+++ practice8/comparison_operator.php
@@ -0,0 +1,40 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- // 21102126 - MM4 -->
+
+<?php
+//* Contoh 1
+$t = 14;
+
+if ($t == 14) {
+    echo "Have a good day!";
+}
+
+//* Contoh 1
+echo ("<br>");
+$t = 14;
+
+if ($t == 14) {
+    echo "Have a good day!";
+}
+
+//* Logical Operators
+echo ("<br>");
+$a = 200;
+$b = 33;
+$c = 500;
+
+if ($a > $b && $c < $a) {
+    echo "Both conditions are true";
+}
+
+//* Logical Operators 2
+echo ("<br>");
+$a = 5;
+
+if ($a == 2 || $a == 3 || $a == 4 || $a == 5 || $a == 6 || $a == 7) {
+    echo "$a is a number between 2 and 7";
+}
+
+
+
+?>

68c64195281c7e01519ae60a18018c8a80fadce7
Muhammad Reyhan Asshidiq Djajasasmita <21102126@ittelkom-pwt.ac.id>
2023-12-17 09:56:10 +0700

    shorthand-if

---
+++ practice8/shorthand_if.php
@@ -0,0 +1,22 @@
+<!-- Muhammad Reyhan Asshidiq Djajasasmita -->
+<!-- // 21102126 - MM4 -->
+
+<?php
+//* Short hand if
+$a = 5;
+
+if ($a < 10)
+    $b = "Hello";
+
+echo $b;
+
+
+//* Short hand if
+echo ("<br>");
+$a = 13;
+
+$b = $a < 10 ? "Hello" : "Good Bye";
+
+echo $b;
+
+?>
